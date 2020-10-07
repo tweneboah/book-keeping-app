8d216f9992b343f8bcb609e55fe449e9a226fb0e
tweneboah <twentekghana@gmail.com>
2020-10-07 09:01:36 +0800

    Final

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,26 @@ app.use(express.json());
 app.use('/api/users', routes.userRouter);
 app.use('/api/books', bookRouter.bookRouter);
 
-//Deployment
-const directory = path.resolve();
-app.use(express.static(path.join(directory, '/frontend/build/index.html')));
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(directory, 'frontend', 'build', 'index.html'))
-);
+// //Deployment
+// const directory = path.resolve();
+// app.use(express.static(path.join(directory, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.resolve(directory, 'frontend', 'build', 'index.html'))
+// );
+const __dirname2 = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname2, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname2, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname2, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 //====Catch Error
 app.use(error.notfoundErrorMiddleware);
 app.use(error.errorMiddlewareHandler);
